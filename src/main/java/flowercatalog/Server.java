d14e5a9d9a7f5995e91fbf27e0a469ba21869579
RahulRj09 <rahul18@navgurukul.org>
2019-09-28 02:10:10 +0530

    Add. PostHandler class

--- src/main/java/flowercatalog/Server.java
+++ src/main/java/flowercatalog/Server.java
@@ -1,21 +1,22 @@
 package flowercatalog;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import javax.swing.filechooser.FileSystemView;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 public class Server {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/", new MyHandler());
+        server.createContext("/comments",new PostHandler());
         server.setExecutor(null);
         server.start();
     }
@@ -24,9 +25,28 @@ public class Server {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             URI requestURI = exchange.getRequestURI();
-            System.out.println(requestURI);
             File root = FileSystemView.getFileSystemView().getHomeDirectory();
-            File file = new File(root + "/Flower-Catalog/src/main/java/"+requestURI);
+            String path = root + "/Flower-Catalog/src/main/java/" + requestURI;
+            File file = new File(path);
+            exchange.sendResponseHeaders(200, file.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                Files.copy(file.toPath(), os);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private static class PostHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            InputStreamReader streamReader = new InputStreamReader(exchange.getRequestBody(), StandardCharsets.UTF_8);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            String query = bufferedReader.readLine();
+            System.out.println(query);
+            File root = FileSystemView.getFileSystemView().getHomeDirectory();
+            String path = root + "/Flower-Catalog/src/main/java/";
+            File file = new File(path);
             exchange.sendResponseHeaders(200, file.length());
             try (OutputStream os = exchange.getResponseBody()) {
                 Files.copy(file.toPath(), os);
@@ -36,3 +56,4 @@ public class Server {
         }
     }
 }
+
